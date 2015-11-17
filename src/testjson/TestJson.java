fd49d064d2b4427eca50852b180465ebc6a3d89d
brenda <brendy2013@gmail.com>
2015-11-17 12:21:07 +0100

    My First Json lesson

--- src/testjson/TestJson.java
+++ src/testjson/TestJson.java
@@ -15,16 +15,33 @@ public class TestJson {
     public static void main(String[] args) {
         // TODO code application logic here
 
-        String json = "{'country' : 'Nigeria' , 'weather' : 'Cloudy' , 'minTemperature' : '28 degree celcius minimum temperature' , 'maxTemperature' : '33 degree celcius maximum temperature' }";
+        String json = "{'country' : 'Nigeria' , 'weather' : 'Weather condition for today: Cloudy'}";
+        String coord = "{'lon' : longitude 8.675277 , 'lat' :latitude 9.081999}";
+        String weather = "{'main' : 'Cloud' , 'description' : 'overcast clouds' , 'icon' : '04N'}";
+        String main = "{'humidity' : The average daily relative humidity for November is around 82% , 'pressure' : pressure is 1013 in Pascal , 'temp_min' : '20 degree Celcius Maximum temperature' , 'temp_max' : '35 degree Celcius maximum temperature'}";
+       
+
         try {
             JSONObject jsonObject = new JSONObject(json);
+            JSONObject jsonObject1 = new JSONObject (coord);
+            JSONObject jsonObject2 = new JSONObject (weather);
+            JSONObject jsonObject3 = new JSONObject (main);
+            
             System.out.println(jsonObject.getString("country"));
+            System.out.println(jsonObject1.getString ("lon"));
+            System.out.println(jsonObject1.getString ("lat"));
             System.out.println(jsonObject.getString("weather"));
-            System.out.println(jsonObject.getString("minTemperature"));
-            if(jsonObject.has("maxTemperature"))
-                 System.out.println(jsonObject.getString("maxTemperature"));
-            else
-                System.out.println(jsonObject.optString("maxTemperature", " Maximum temperature Not  Available"));
+            System.out.println(jsonObject2.getString ("description"));
+            System.out.println(jsonObject3.getString ("humidity"));
+            System.out.println(jsonObject3.getString ("pressure"));
+            System.out.println(jsonObject3.getString ("temp_min"));
+            System.out.println(jsonObject3.getString ("temp_max"));
+            
+           
+           // if(jsonObject.has("temp_max"))
+                 //System.out.println(jsonObject.getString("temp_max"));
+           // else
+              //  System.out.println(jsonObject.optString("maxTemperature", " Maximum temperature Not  Available"));
 
         } catch (JSONException e) {
             e.printStackTrace();
